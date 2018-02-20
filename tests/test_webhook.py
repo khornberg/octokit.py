10933b72811a4d8aa7e54ae81d982f4d2d0f04dd
Kyle Hornberg <kyle.hornberg@gmail.com>
2018-02-20 09:34:14 -0600

    Test: Use string instead of json.dumps
    json.dumps on Python 3.4 and 3.5 produces differnt signatures

--- tests/test_webhook.py
+++ tests/test_webhook.py
@@ -94,7 +94,7 @@ class TestWebhook(object):
 
     def test_verify_ping_event(self):
         headers = {
-            'X-Hub-Signature': 'sha1=02ad50e35622a4cf597de5fb86f4d59520f5adda',
+            'X-Hub-Signature': 'sha1=76b55589eeb1d5609a01922fc9a52475cf746a5b',
             'X-GitHub-Event': 'ping',
             'X-GitHub-Delivery': '72d3162f-cc78-11e3-81ab-4c9367dc0958',
             'User-Agent': 'GitHub-Hookshot/',
@@ -110,8 +110,10 @@ class TestWebhook(object):
                 }
             }
         )
+        payload = '{"hook": {"events": ["pull_request"], "app_id": 42, "id": 11, "active": true, "type": "App"}}'
         secret = 'secret'
-        assert webhook.verify(headers, payload, secret, events=['*'], return_app_id=True) == 42
+        app_id = webhook.verify(headers, payload, secret, events=['*'], return_app_id=True)
+        assert app_id == 42
 
     def test_can_request_app_id_be_returned_on_non_ping_events(self):
         headers = {
