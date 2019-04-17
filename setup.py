de7116c223db782aae50935da1dfe369d7428f20
Kyle Hornberg <khornberg@users.noreply.github.com>
2019-04-16 21:04:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*names, **kwargs):
 
 setup(
     name='octokitpy',
-    version='0.10.2',
+    version='0.11.0',
     license='MIT license',
     description='Python client for GitHub API',
     long_description='%s\n%s' % (
